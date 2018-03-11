0b8d73b74660532ece92b240567bd39f48a9c871
Lucas Carvalho <lucasbr023@gmail.com>
2018-03-11 11:00:27 -0300

    set model campain and set model member

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-  extend Devise::Models
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :campaigns, dependent: :destroy
 end
